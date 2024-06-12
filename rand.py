5ef2372ab2f9b05c6909c4450567a4cdb4f00b48
Mahenina <kylleandriamahenina@gmail.com>
2024-06-12 21:23:39 +0300

    commitF

---
+++ rand.py
@@ -0,0 +1,22 @@
+import random,time,os
+x=11111111111111
+y=99999999999999
+color=[
+'\033[0;96m' ,
+'\033[1;37m ',
+'\033[1;92m' ,
+'\033[1;35m' ,
+'\033[1;33m' ,
+'\033[1;34m' ,
+'\033[1;96m' ,
+'\033[1;31m'
+]
+
+
+limit=int(input("limit:"))
+print("      ")
+for i in range(0,limit):
+	c=random.choice(color)
+	r=random.randint(x,y)
+	time.sleep(0.5)
+	print(c,"#321*",r,"#")
